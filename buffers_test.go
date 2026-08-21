443f401e89b82c9671d0fb571f01e89ae272b376
Alex <alex.migolin@livekit.io>
2026-08-21 16:09:19 -0700

    Allow never writing partial frames, even when closing (#80)

--- buffers_test.go
+++ buffers_test.go
@@ -46,3 +46,30 @@ func TestFullFrames(t *testing.T) {
 		{7},
 	}, got)
 }
+
+func TestFullFramesDropTailOnClose(t *testing.T) {
+	var got []PCM16Sample
+	w := FullFrames(NewPCM16FrameWriter(&got, 8000), 2, DropTailOnClose[PCM16Sample]())
+	for _, f := range []PCM16Sample{
+		{},
+		{1},
+		{2, 3},
+		{4, 5, 6},
+		{7},
+	} {
+		err := w.WriteSample(f)
+		require.NoError(t, err)
+	}
+	require.Equal(t, []PCM16Sample{
+		{1, 2},
+		{3, 4},
+		{5, 6},
+	}, got)
+	err := w.Close()
+	require.NoError(t, err)
+	require.Equal(t, []PCM16Sample{
+		{1, 2},
+		{3, 4},
+		{5, 6},
+	}, got)
+}
